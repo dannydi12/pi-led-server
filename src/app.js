06ee42897851974466238f64e410e17c6c530923
dannydi12 <danieldivenere@gmail.com>
2020-02-11 02:49:35 +0000

    organized express code

--- src/app.js
+++ src/app.js
@@ -3,17 +3,14 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const reset = require('./reset');
-const { spawn } = require('child_process');
-const { basePath } = require('./store');
 const { NODE_ENV } = require('../config');
+const showRouter = require('./show/showRouter');
 
 const app = express();
 
 app.use(morgan(NODE_ENV === 'production' ? 'tiny' : 'common'));
 app.use(helmet());
 app.use(cors());
-app.use(reset);
 app.use((error, req, res, next) => {
   let response;
   if (NODE_ENV === 'production') {
@@ -30,36 +27,7 @@ app.use((error, req, res, next) => {
   res.status(500).send(response);
 });
 
-function convertHex(h) {
-  let r = 0, g = 0, b = 0;
-
-  if (h.length == 3) {
-    r = "0x" + h[0] + h[0];
-    g = "0x" + h[1] + h[1];
-    b = "0x" + h[2] + h[2];
-
-  } else if (h.length == 6) {
-    r = "0x" + h[0] + h[1];
-    g = "0x" + h[2] + h[3];
-    b = "0x" + h[4] + h[5];
-  }
-  return {r: +r, g: +g, b: +b} ;
-}
-
-app.post('/routines', (req, res) => {
-  const { name, brightness = 200, color = 'f1c40f' } = req.query;
-  const {r, g, b} = convertHex(color)
-  display = spawn('python', [`${basePath}/routines/${name}.py`, `-l ${brightness}`, `-r ${r}`, `-g ${g}`, `-b ${b}`]);
-  display.stdout.on('data', (data) => console.log(`stdout: ${data}`));
-  display.stderr.on('data', (data) => console.error(`stderr: ${data}`));
-  isDisplaying = true
-  display.on('close', (code) => {
-    isDisplaying = false;
-    console.log(`child process exited with code ${code}`)
-  });
-
-  res.send('Hello, world!');
-});
+app.use('/show', showRouter)
 
 app.post('/kill', (req, res) => {
   display.kill('SIGINT');
---
+++ src/show/showRouter.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const { spawn } = require('child_process');
+const { basePath } = require('../store');
+const reset = require('../reset');
+
+const showRouter = express.Router();
+
+function convertHex(h) {
+  let r = 0, g = 0, b = 0;
+
+  if (h.length == 3) {
+    r = "0x" + h[0] + h[0];
+    g = "0x" + h[1] + h[1];
+    b = "0x" + h[2] + h[2];
+
+  } else if (h.length == 6) {
+    r = "0x" + h[0] + h[1];
+    g = "0x" + h[2] + h[3];
+    b = "0x" + h[4] + h[5];
+  }
+  return {r: +r, g: +g, b: +b} ;
+}
+
+showRouter.route('/')
+  .post(reset, (req, res) => {
+    const { name, brightness = 200, color = 'f1c40f' } = req.query;
+    const { r, g, b } = convertHex(color)
+    display = spawn('python', [`${basePath}/routines/${name}.py`, `-l ${brightness}`, `-r ${r}`, `-g ${g}`, `-b ${b}`]);
+    display.stdout.on('data', (data) => console.log(`stdout: ${data}`));
+    display.stderr.on('data', (data) => console.error(`stderr: ${data}`));
+    isDisplaying = true
+    display.on('close', (code) => {
+      isDisplaying = false;
+      console.log(`child process exited with code ${code}`)
+    });
+    res.send('Hello, world!');
+  });
+
+module.exports = showRouter;
