78ba2f9a66c03a336fd08453fb48a78e7796b35e
dannydi12 <danieldivenere@gmail.com>
2020-03-22 00:49:21 +0000

    refactored reset middleware

--- src/reset.js
+++ src/reset.js
@@ -1,11 +1,9 @@
 function reset(req, res, next) {
   const clear = new Promise(resolve => {
-    const led = {
-      display: req.app.get('display')
-    }
-    if (led.display.exitCode == null) {
-      led.display.kill('SIGINT');
-      led.display.on('close', resolve);
+    const display = req.app.get('display')
+    if (display.exitCode == null) {
+      display.kill('SIGINT');
+      display.on('close', resolve);
     }
     else {
       resolve();
